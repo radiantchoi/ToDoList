3f8812e2626952d719d17ed62eac2b3313831ffd
Gordon Choi <thefirate@gmail.com>
2021-05-30 01:57:15 +0900

    Add model and related view controller

---
+++ ToDoList/Model/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Gordon Choi on 2021/05/30.
+//
+
+import Foundation
+
+struct ToDo {
+    
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+}
