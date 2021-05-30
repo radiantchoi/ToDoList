1f2d6b1f7e1a10a72d17ffb57b9fb49bea0e728e
Gordon Choi <thefirate@gmail.com>
2021-05-31 01:04:25 +0900

    Set edit and delete control for main screen

--- ToDoList/ViewController/ToDoTableViewController.swift
+++ ToDoList/ViewController/ToDoTableViewController.swift
@@ -23,8 +23,12 @@ extension ToDoTableViewController {
         } else {
             todos = ToDo.loadSampleToDos()
         }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
     }
+}
 
+extension ToDoTableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,7 +39,6 @@ extension ToDoTableViewController {
         return todos.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
 
@@ -45,26 +48,23 @@ extension ToDoTableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            todos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+        /*
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        */
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
