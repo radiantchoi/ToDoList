4425797f6bf9f337a99880e87a063f7cb0fcbd22
Gordon Choi <thefirate@gmail.com>
2021-06-01 03:34:04 +0900

    Setup full function of custom cell

--- ToDoList/ViewController/ToDoTableViewController.swift
+++ ToDoList/ViewController/ToDoTableViewController.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 
-class ToDoTableViewController: UITableViewController {
+class ToDoTableViewController: UITableViewController, ToDoCellDelegate {
     
     var todos = [ToDo]()
+    
+    func checkMarkTapped(sender: ToDoCell) {
+        if let indexPath = tableView.indexPath(for: sender) {
+            var todo = todos[indexPath.row]
+            todo.isComplete = !todo.isComplete
+            todos[indexPath.row] = todo
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
     
 }
 
@@ -39,11 +48,13 @@ extension ToDoTableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier") as? ToDoCell else {
             fatalError("Could not dequeue a cell")
         }
 
+        cell.delegate = self
+        
         let todo = todos[indexPath.row]
         cell.titleLabel?.text = todo.title
         cell.isCompleteButton.isSelected = todo.isComplete
@@ -82,7 +93,9 @@ extension ToDoTableViewController {
         return true
     }
     */
-    
+}
+
+extension ToDoTableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "EditToDo", let navController = segue.destination as? UINavigationController, let todoDetailTableViewController = navController.topViewController as? ToDoDetailTableViewController {
